867537c8651e1f5781b755425f87a315d41b39af
toxa <Saint2202*>
2020-09-13 14:44:36 +0300

    Create SearchBar and animated background

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank=() => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Anthony, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'№5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;  
